c164435d66d9ddaec798ec2aa0223c71ca3c8635
bruno.gianesi <bruno.gianesi@ifood.com.br>
2021-08-02 00:38:21 -0300

    Tentativa

--- ConexaoEPOS.cpp
+++ ConexaoEPOS.cpp
@@ -1,13 +1,8 @@
 //------------------------------------------------------------------------------
-// File: simple.cpp
-// Desc: A bare bones example of using the EPOSControl library
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include "EPOSControl/EPOSControl.h"
+#include "EPOSControl/include/EPOSControl/EPOSControl.h"
 
 //------------------------------------------------------------------------------
 #if !defined(WIN32) || defined(__CYGWIN__)
@@ -23,29 +18,21 @@ void catchSignal( int sig )
 int main()
 {
 #if !defined(WIN32) || defined(__CYGWIN__)
-    // install signal handler for manual break
+    // Comunicação
     signal( SIGTERM, catchSignal );
     signal( SIGINT, catchSignal );
 #endif
     
-    // Start up the EPOSControl library
-    if ( !EPOS_InitLibrary() )
-    {
-        fprintf( stderr, "Error: Unable to open EPOSControl library\n" );
-        return -1;
-    }
+    // Inicia a Biblioteca
+    EPOS_InitLibrary()
     
     CANChannel* pChannel = EPOS_OpenCANChannel( "libCanUSBDriver.so", "32", eBR_1M );
-    if ( NULL == pChannel )
-    {
-        fprintf( stderr, "Error: Unable to open CAN bus channel\n" );
-        return -1;
-    }
+
 
     // Wait for Ctrl-C
     pause();
     
-    // Shut down the EPOSControl library
+    // Finaliza a Biblioteca
     EPOS_CloseCANChannel( pChannel );
     EPOS_DeinitLibrary();
     
